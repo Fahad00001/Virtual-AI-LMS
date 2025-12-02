7306f60be11a3ebffdd20f9917fb0d12cdb23ea4
Fahad <145900785+Fahad00001@users.noreply.github.com>
2025-12-02 12:40:06 +0530

    Refactor

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
 import express from "express"
-import dotenv  from 'dotenv'
+import dotenv from 'dotenv'
 import connectDb from "./config/connectDB.js"
 import cookieParser from "cookie-parser"
 import authRouter from "./route/authRoute.js"
@@ -8,36 +8,30 @@ import userRouter from "./route/userRoute.js"
 import courseRouter from "./route/courseRoute.js"
 import paymentRouter from "./route/paymentRoute.js"
 import reviewRouter from "./route/reviewRoute.js"
+
 dotenv.config()
 
-const port=process.env.PORT
-const app=express()
+const port = process.env.PORT
+const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-<<<<<<< HEAD
-    origin: "http://localhost:5173",
-=======
-    origin: "https://ai-powerd-lms-3.onrender.com",
->>>>>>> f9debe746dfb321d029133dca084d0ca63a5c25a
+    origin: "http://localhost:5173",   // <--- replace this
     credentials: true,
 }))
 
 app.use("/api/auth", authRouter)
 app.use("/api/user", userRouter)
-app.use("/api/course",courseRouter)
-app.use("/api/order",paymentRouter)
-app.use("/api/review",reviewRouter)
+app.use("/api/course", courseRouter)
+app.use("/api/order", paymentRouter)
+app.use("/api/review", reviewRouter)
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("hello from server")
-
 })
-app.listen(port,()=>{
-    // console.log("server started");
-    console.log(`Server started on port ${port}`);
-    connectDb()
 
-    
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`)
+    connectDb()
 })
